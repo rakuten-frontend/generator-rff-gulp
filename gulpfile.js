f5dcca562c7a6686163c15c16df73156f03fb2ef
Hiroyuki Tanjo <expheno@gmail.com>
2016-02-24 19:37:34 +0900

    Mock GitHub response with local fixtures

--- gulpfile.js
+++ gulpfile.js
@@ -32,11 +32,10 @@ gulp.task('pre-test', function () {
 gulp.task('test', ['pre-test'], function (cb) {
   var mochaErr;
 
-  gulp.src('test/**/*.js')
+  gulp.src('test/specs/*.js')
     .pipe(plumber())
     .pipe(mocha({
-      reporter: 'spec',
-      timeout: 10000
+      reporter: 'spec'
     }))
     .on('error', function (err) {
       mochaErr = err;
--- test/specs/general.js
+++ test/specs/general.js
@@ -4,11 +4,25 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
+var nock = require('nock');
 
 describe('Generator', function () {
+  before(function () {
+    nock('https://github.com')
+      .persist()
+      .get('/rakuten-frontend/rff-gulp/raw/master/package.json')
+      .replyWithFile(200, path.join(__dirname, '../fixtures/package.json'))
+      .get('/rakuten-frontend/rff-gulp/releases/download/v0.0.0/rff-gulp-v0.0.0.zip')
+      .replyWithFile(200, path.join(__dirname, '../fixtures/rff-gulp-v0.0.0.zip'));
+  });
+
+  after(function () {
+    nock.cleanAll();
+  });
+
   describe('with default options', function () {
     before(function (done) {
-      helpers.run(path.join(__dirname, '../generators/app'))
+      helpers.run(path.join(__dirname, '../../generators/app'))
         .on('end', done);
     });
 
@@ -21,12 +35,12 @@ describe('Generator', function () {
 
   describe('without "ready" answer', function () {
     before(function (done) {
-      helpers.run(path.join(__dirname, '../generators/app'))
+      helpers.run(path.join(__dirname, '../../generators/app'))
         .withPrompts({ready: false})
         .on('end', done);
     });
 
-    it('cancels creating files', function () {
+    it('doesn\'t create files', function () {
       assert.noFile([
         'package.json'
       ]);
