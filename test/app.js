e531a2ecda902918ebcd9e6154c987682ae74089
Hiroyuki Tanjo <expheno@gmail.com>
2016-02-24 11:35:22 +0900

    Update test config

--- test/app.js
+++ test/app.js
@@ -1,19 +1,21 @@
+/* eslint-env mocha */
 'use strict';
+
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
 describe('generator-rff-gulp:app', function () {
+  this.timeout(10000);
+
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'package.json'
     ]);
   });
 });
