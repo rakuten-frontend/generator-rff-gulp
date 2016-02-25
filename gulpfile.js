e6af11a1b1ff837779b964754a512ed15fd613c5
Hiroyuki Tanjo <expheno@gmail.com>
2016-02-25 09:13:21 +0900

    Refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,45 @@
 'use strict';
-var path = require('path');
+
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var excludeGitignore = require('gulp-exclude-gitignore');
-var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
-var plumber = require('gulp-plumber');
-var coveralls = require('gulp-coveralls');
+var $ = require('gulp-load-plugins')();
+var path = require('path');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+  return gulp.src([
+    '*.js',
+    'generators/app/*.js',
+    'test/specs/*.js'
+  ])
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
 });
 
 gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
+  $.nsp({package: path.resolve('package.json')}, cb);
 });
 
 gulp.task('pre-test', function () {
   return gulp.src('generators/**/*.js')
-    .pipe(istanbul({
+    .pipe($.istanbul({
       includeUntested: true
     }))
-    .pipe(istanbul.hookRequire());
+    .pipe($.istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function (cb) {
   var mochaErr;
 
   gulp.src('test/specs/*.js')
-    .pipe(plumber())
-    .pipe(mocha({
+    .pipe($.plumber())
+    .pipe($.mocha({
       reporter: 'spec',
       timeout: 10000
     }))
     .on('error', function (err) {
       mochaErr = err;
     })
-    .pipe(istanbul.writeReports())
+    .pipe($.istanbul.writeReports())
     .on('end', function () {
       cb(mochaErr);
     });
@@ -51,9 +49,8 @@ gulp.task('coveralls', ['test'], function () {
   if (!process.env.CI) {
     return;
   }
-
   return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+    .pipe($.coveralls());
 });
 
 gulp.task('prepublish', ['nsp']);
